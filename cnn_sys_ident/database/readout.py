70c573e847f995fa764f008f4268d7f9bc82b0f5
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2018-10-24 11:33:16 +0200

    Analyses for paper

--- cnn_sys_ident/database/readout.py
+++ cnn_sys_ident/database/readout.py
@@ -22,6 +22,13 @@ class SpatialSparseXFeatureDense(RegularizableComponent):
         ('init_masks', 'ENUM("sta", "rand") # initializaton method for masks'),
     ])
 
+class SpatialSparseXFeatureDenseSeparate(RegularizableComponent):
+    _regularization_parameters = ['mask_sparsity', 'feature_l2']
+    _parameters = OrderedDict([
+        ('positive_feature_weights', 'boolean # enforce positive feature weights?'),
+        ('init_masks', 'ENUM("sta", "rand") # initializaton method for masks'),
+    ])
+
 class SpatialXFeatureJointL1Transfer(RegularizableComponent):
     _regularization_parameters = ['readout_sparsity']
     _parameters = OrderedDict([
